5c2d8b0a464d8f03f929ffa8ee95b332c5bb9e7b
Tierra <50035689+RuckerTierra@users.noreply.github.com>
2022-02-28 00:41:39 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+// Temperature converter 
+// Part A: convert from Celsius to Fahrenheit
+// step 1 Get a number prompt()
+// step 2 take the number (num)
+// 3 parseInt() the number (num)
+// 4 convert to Fahrenheit (by adding 2)
+// 5 display the converted number 
+
+
+// // let num1 = prompt("Pick a number"); 
+// let num2 = parseInt(num1) + 32;
+
+
+// // convertToFahrenheit(){}
+// // convertToFahrenheit();
+// console.log(`${num1} degrees Celsius is ${num2} degrees Fahrenheit`); 
+
+let demoDisplay = document.getElementById("demo").innerHTML = "howdy folks";
+let num1 = document.getElementById("inputNum").value;
+
+convert(){
+    console.log(num1);
+}
+convert();
+
+
+
+// Part B: Convert from Fahrenheit to Celsius
+// (0°F − 32) × 5/9 = -17.78°C
+// (num3 - 32) * 5/9 = num4 
